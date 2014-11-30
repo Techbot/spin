a1291945e380ceeed8a6b13efdaee02eba27c726
Christopher Pitt <cgpitt@gmail.com>
2014-11-30 19:54:55 +0200

    Refactoring

--- app/Http/Handler/IndexHandler.php
+++ app/Http/Handler/IndexHandler.php
@@ -9,20 +9,6 @@ class IndexHandler
      */
     public function index()
     {
-        return "<script>
-
-    var connection = new WebSocket('ws://127.0.0.1:4002');
-
-    connection.addEventListener('open', function(event) {
-        console.log('Connection established!');
-    });
-
-    connection.addEventListener('message', function(event) {
-        console.log(event.data);
-    });
-
-    console.log('waiting for connection...');
-
-</script>";
+        return file_get_contents("resources/views/index/index.html");
     }
 }
